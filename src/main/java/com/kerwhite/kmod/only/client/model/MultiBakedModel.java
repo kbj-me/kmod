8e22d9179127950797b81283ba9dc23f0b07649d
author <ker_white@163.com>
2025-11-23 11:13:57 +0800

    hhh

--- src/main/java/com/kerwhite/kmod/only/client/model/MultiBakedModel.java
+++ src/main/java/com/kerwhite/kmod/only/client/model/MultiBakedModel.java
@@ -36,45 +36,59 @@ import java.util.Objects;
 public class MultiBakedModel implements BakedModel
 {
     private final List<BakedModel> models = new ArrayList<>();
-    public MultiBakedModel(){}
+
+    public MultiBakedModel()
+    {
+    }
+
     private final ItemOverrides overrides = new ItemOverrides()
     {
+        /*
+
+         */
         @Override
         public @Nullable BakedModel resolve(BakedModel model, ItemStack stack, @Nullable ClientLevel level, @Nullable LivingEntity entity, int seed)
         {
-            List<BakedModel> $models = new ArrayList<>();
-            CompoundTag tag = stack.getOrCreateTag();
-            if (tag.contains("multimodel") && tag.contains("modelcount") && tag.contains("models"))
+            try
             {
-                ModelManager manager = Minecraft.getInstance().getModelManager();
-                int count = tag.getInt("modelcount");
-                CompoundTag models = tag.getCompound("models");
-                for (int i = 0; i < count; i++)
+                List<BakedModel> $models = new ArrayList<>();
+                CompoundTag tag = stack.getOrCreateTag();
+                if (tag.contains("multimodel") && tag.contains("modelcount") && tag.contains("models"))
                 {
-                    String modelString = "model";
-                    String modelModId = modelString.concat(Integer.toString(i)).concat("_modid");
-                    String modelLocation = modelString.concat(Integer.toString(i)).concat("_location");
-                    if (models.contains(modelModId) && models.contains(modelLocation))
+                    ModelManager manager = Minecraft.getInstance().getModelManager();
+                    int count = tag.getInt("modelcount");
+                    CompoundTag models = tag.getCompound("models");
+                    for (int i = 0; i < count; i++)
                     {
-                        String $modelModId = models.getString(modelModId);
-                        String $modelLocation = models.getString(modelLocation);
-                        ResourceLocation $$modelLocation = ResourceLocation.fromNamespaceAndPath($modelModId, $modelLocation);
-                        ModelResourceLocation modelResourceLocation = new ModelResourceLocation($$modelLocation,"inventory");
-                        BakedModel $model = manager.getModel(modelResourceLocation);
-                        $models.add($model);
+                        String modelString = "model";
+                        String modelModId = modelString.concat(Integer.toString(i)).concat("_modid");
+                        String modelLocation = modelString.concat(Integer.toString(i)).concat("_location");
+                        if (models.contains(modelModId) && models.contains(modelLocation))
+                        {
+                            String $modelModId = models.getString(modelModId);
+                            String $modelLocation = models.getString(modelLocation);
+                            ResourceLocation $$modelLocation = ResourceLocation.fromNamespaceAndPath($modelModId, $modelLocation);
+                            ModelResourceLocation modelResourceLocation = new ModelResourceLocation($$modelLocation, "inventory");
+                            BakedModel $model = manager.getModel(modelResourceLocation);
+                            $models.add($model);
+                        }
+                        else
+                        {
+                            break;
+                        }
                     }
-                    else
-                    {
-                        break;
-                    }
+                    return new MultiBakedModel($models.toArray(new BakedModel[0]));
                 }
-                return new MultiBakedModel($models.toArray(new BakedModel[0]));
+                return super.resolve(model, stack, level, entity, seed);
             }
-            return super.resolve(model, stack, level, entity, seed);
+            catch (Exception e)
+            {
+                return super.resolve(model, stack, level, entity, seed);
+            }
         }
     };
 
-    public MultiBakedModel(BakedModel... models)
+    public MultiBakedModel(@NotNull BakedModel... models)
     {
         Objects.requireNonNull(models);
         for (BakedModel model : models)
@@ -132,14 +146,19 @@ public class MultiBakedModel implements BakedModel
     @Override
     public @NotNull BakedModel applyTransform(@NotNull ItemDisplayContext transformType, @NotNull PoseStack poseStack, boolean applyLeftHandTransform)
     {
-        List<BakedModel> transfromModels = new ArrayList<>();
-        this.models.forEach((model) ->
+        for(BakedModel model : this.models)
         {
-            poseStack.pushPose();
-            transfromModels.add(model.applyTransform(transformType, poseStack, applyLeftHandTransform));
-            poseStack.popPose();
-        });
-        return new MultiBakedModel(transfromModels.toArray(new BakedModel[0]));
+            model.applyTransform(transformType, poseStack, applyLeftHandTransform);
+        }
+        return this;
+//        List<BakedModel> transfromModels = new ArrayList<>();
+//        this.models.forEach((model) ->
+//        {
+//            poseStack.pushPose();
+//            transfromModels.add(model.applyTransform(transformType, poseStack, applyLeftHandTransform));
+//            poseStack.popPose();
+//        });
+//        return new MultiBakedModel(transfromModels.toArray(new BakedModel[0]));
     }
 
     @Override
@@ -147,7 +166,8 @@ public class MultiBakedModel implements BakedModel
     {
         return this.overrides;
     }
-    @Mod.EventBusSubscriber(modid = kmod.MODID,bus = Mod.EventBusSubscriber.Bus.MOD,value = Dist.CLIENT)
+
+    @Mod.EventBusSubscriber(modid = kmod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class EventHandler
     {
         @SubscribeEvent
@@ -155,7 +175,7 @@ public class MultiBakedModel implements BakedModel
         {
             Map<ResourceLocation, BakedModel> modelRegistry = event.getModels();
             ModelResourceLocation location = new ModelResourceLocation(BuiltInRegistries.ITEM.getKey(ItemRegister.MULTI_MODEL_ITEM.get()), "inventory");
-            modelRegistry.put(location,new MultiBakedModel());
+            modelRegistry.put(location, new MultiBakedModel());
         }
     }
 }
